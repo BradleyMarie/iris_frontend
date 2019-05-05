ffac53f53f08361cbcc2e69f8eef1c39c9dfcd47
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2019-05-05 00:54:58 -0700

    Tweak error text

--- src/matrix_manager.cc
+++ src/matrix_manager.cc
@@ -84,9 +84,9 @@ void MatrixManager::Rotate(float_t theta, float_t x, float_t y, float_t z) {
       std::cerr << "ERROR: Invalid z parameter to Rotate " << x << std::endl;
       exit(EXIT_FAILURE);
     case ISTATUS_INVALID_ARGUMENT_COMBINATION_00:
-      std::cerr
-          << "ERROR: One of the x, y, or z parameter of Rotate must be non-zero"
-          << std::endl;
+      std::cerr << "ERROR: One of the x, y, or z parameters of Rotate must be "
+                   "non-zero"
+                << std::endl;
       exit(EXIT_FAILURE);
     case ISTATUS_ALLOCATION_FAILED:
       std::cerr << "ERROR: Allocation failed" << std::endl;
