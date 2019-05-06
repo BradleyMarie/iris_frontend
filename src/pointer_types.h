6c8bacb89277df337a88365a380be466685d308b
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2019-05-05 19:13:18 -0700

    Implement UniquePtr and add more types

--- src/pointer_types.h
+++ src/pointer_types.h
@@ -1,8 +1,23 @@
+#include "iris_camera/iris_camera.h"
 #include "iris_physx/iris_physx.h"
 #include "src/shared_ptr.h"
+#include "src/unique_ptr.h"
 
 namespace iris {
 
+typedef SharedPtr<BSDF, BsdfRetain, BsdfRelease> Bsdf;
+typedef UniquePtr<CAMERA, CameraFree> Camera;
+typedef UniquePtr<COLOR_INTEGRATOR, ColorIntegratorFree> ColorIntegrator;
+typedef SharedPtr<EMISSIVE_MATERIAL, EmissiveMaterialRetain,
+                  EmissiveMaterialRelease>
+    EmissiveMaterial;
+typedef UniquePtr<FRAMEBUFFER, FramebufferFree> Framebuffer;
+typedef UniquePtr<INTEGRATOR, IntegratorFree> Integrator;
+typedef SharedPtr<MATERIAL, MaterialRetain, MaterialRelease> Material;
 typedef SharedPtr<MATRIX, MatrixRetain, MatrixRelease> Matrix;
+typedef SharedPtr<LIGHT, LightRetain, LightRelease> Light;
+typedef UniquePtr<RANDOM, RandomFree> Random;
+typedef SharedPtr<REFLECTOR, ReflectorRetain, ReflectorRelease> Reflector;
+typedef SharedPtr<SPECTRUM, SpectrumRetain, SpectrumRelease> Spectrum;
 
 }  // namespace iris
---
+++ src/unique_ptr.h
@@ -0,0 +1,44 @@
+namespace iris {
+
+template <typename Type, void (*Release)(Type*)>
+class UniquePtr {
+ public:
+  UniquePtr(const UniquePtr& other) = delete;
+  UniquePtr& operator=(const UniquePtr& other) = delete;
+
+  UniquePtr() : m_ptr(nullptr) {}
+  UniquePtr(UniquePtr&& other) : m_ptr(other.m_ptr) { other.m_ptr = nullptr; }
+  ~UniquePtr() { Release(m_ptr); }
+  UniquePtr& operator=(UniquePtr&& other) {
+    if (this != &other) {
+      m_ptr = other.m_ptr;
+      other.m_ptr = nullptr;
+    }
+    return *this;
+  }
+
+  Type* get() { return m_ptr; }
+  const Type* get() const { return m_ptr; }
+
+  void reset() {
+    Release(m_ptr);
+    m_ptr = nullptr;
+  }
+
+  Type* detach() {
+    auto result = m_ptr;
+    m_ptr = nullptr;
+    return result;
+  }
+
+  Type** release_and_get_address() {
+    Release(m_ptr);
+    m_ptr = nullptr;
+    return &m_ptr;
+  }
+
+ private:
+  Type* m_ptr;
+};
+
+}  // namespace iris
