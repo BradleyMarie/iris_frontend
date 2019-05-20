f27528535df6a1987d4d0f658a67d6f2986b51ab
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2019-05-19 19:34:59 -0700

    Reset matrix in scene parser

--- src/scene_parser.cc
+++ src/scene_parser.cc
@@ -9,6 +9,9 @@ std::pair<Scene, std::vector<Light>> ParseScene(
     ColorIntegrator& color_integrator) {
   std::cerr << "ERROR: ParseScene not implemented" << std::endl;
   exit(EXIT_FAILURE);
+
+  matrix_manager.ActiveTransform(MatrixManager::ALL_TRANSFORMS);
+  matrix_manager.Identity();
 }
 
 }  // namespace iris
