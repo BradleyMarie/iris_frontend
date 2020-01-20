8183f58cf010af985edf940057b0f719efab80c0
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2020-01-19 16:25:19 -0800

    Add call to ParseDistant

--- src/lights/parser.cc
+++ src/lights/parser.cc
@@ -1,6 +1,7 @@
 #include "src/lights/parser.h"
 
 #include "src/common/call_directive.h"
+#include "src/lights/distant.h"
 #include "src/lights/point.h"
 
 namespace iris {
@@ -8,8 +9,10 @@ namespace iris {
 Light ParseLight(const char* base_type_name, Tokenizer& tokenizer,
                  SpectrumManager& spectrum_manager,
                  const Matrix& model_to_world) {
-  return CallDirective<Light, 1, SpectrumManager&, const Matrix&>(
-      base_type_name, tokenizer, {std::make_pair("point", ParsePoint)},
+  return CallDirective<Light, 2, SpectrumManager&, const Matrix&>(
+      base_type_name, tokenizer,
+      {std::make_pair("point", ParsePoint),
+       std::make_pair("distant", ParseDistant)},
       spectrum_manager, model_to_world);
 }
 
