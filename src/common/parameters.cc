b048b1f229ea9b9da48ccce3e683795301ff371b
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2020-07-29 23:05:45 -0700

    Implement MatchParameters as functions inside Parameters

--- src/common/parameters.cc
+++ src/common/parameters.cc
@@ -2,6 +2,62 @@
 
 namespace iris {
 
+namespace {
+
+void MatchParameter(absl::string_view base_type_name,
+                    absl::string_view type_name,
+                    absl::Span<ParamMatcher* const> param_matchers,
+                    Parameter& parameter,
+                    std::vector<Parameter>* unhandled_parameters) {
+  bool found = false;
+  for (auto& current : param_matchers) {
+    if (current->Match(base_type_name, type_name, parameter)) {
+      found = true;
+      break;
+    }
+  }
+  if (!found) {
+    if (!unhandled_parameters) {
+      std::cerr << "ERROR: Unrecognized or misconfigured parameter to "
+                << type_name << " " << base_type_name << ": " << parameter.first
+                << std::endl;
+      exit(EXIT_FAILURE);
+    }
+
+    unhandled_parameters->push_back(parameter);
+  }
+}
+
+void MatchParametersF(absl::string_view base_type_name,
+                      absl::string_view type_name, Tokenizer& tokenizer,
+                      absl::Span<ParamMatcher* const> param_matchers,
+                      std::vector<Parameter>* unhandled_parameters) {
+  for (auto parameter = ParseNextParam(tokenizer); parameter.has_value();
+       parameter = ParseNextParam(tokenizer)) {
+    MatchParameter(base_type_name, type_name, param_matchers, *parameter,
+                   unhandled_parameters);
+  }
+  for (auto matcher : param_matchers) {
+    matcher->Validate(base_type_name, type_name);
+  }
+}
+
+void MatchParameters(absl::string_view base_type_name,
+                     absl::string_view type_name,
+                     std::vector<Parameter>& parameters,
+                     absl::Span<ParamMatcher* const> param_matchers,
+                     std::vector<Parameter>* unhandled_parameters) {
+  for (auto& parameter : parameters) {
+    MatchParameter(base_type_name, type_name, param_matchers, parameter,
+                   unhandled_parameters);
+  }
+  for (auto matcher : param_matchers) {
+    matcher->Validate(base_type_name, type_name);
+  }
+}
+
+}  // namespace
+
 Parameters::Parameters(absl::string_view base_type_name,
                        absl::string_view type_name, Tokenizer& tokenizer)
     : m_base_type_name(base_type_name),
@@ -39,13 +95,12 @@ Parameters Parameters::MatchAllowUnusedImpl(
   assert(m_tokenizer || m_unused_parameters);
   std::vector<Parameter> unused_parameters;
   if (m_tokenizer) {
-    MatchParameters(m_base_type_name, m_type_name, *m_tokenizer, param_matchers,
-                    &unused_parameters);
+    MatchParametersF(m_base_type_name, m_type_name, *m_tokenizer,
+                     param_matchers, &unused_parameters);
     m_tokenizer = nullptr;
   } else {
-    MatchParameters(m_base_type_name, m_type_name,
-                    absl::MakeSpan(*m_unused_parameters), param_matchers,
-                    &unused_parameters);
+    MatchParameters(m_base_type_name, m_type_name, *m_unused_parameters,
+                    param_matchers, &unused_parameters);
     m_unused_parameters = absl::nullopt;
   }
   return Parameters(m_base_type_name, m_type_name,
@@ -55,12 +110,12 @@ Parameters Parameters::MatchAllowUnusedImpl(
 void Parameters::MatchImpl(absl::Span<ParamMatcher* const> param_matchers) {
   assert(m_tokenizer || m_unused_parameters);
   if (m_tokenizer) {
-    MatchParameters(m_base_type_name, m_type_name, *m_tokenizer,
-                    param_matchers);
+    MatchParametersF(m_base_type_name, m_type_name, *m_tokenizer,
+                     param_matchers, nullptr);
     m_tokenizer = nullptr;
   } else {
-    MatchParameters(m_base_type_name, m_type_name,
-                    absl::MakeSpan(*m_unused_parameters), param_matchers);
+    MatchParameters(m_base_type_name, m_type_name, *m_unused_parameters,
+                    param_matchers, nullptr);
     m_unused_parameters = absl::nullopt;
   }
 }
