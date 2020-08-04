beba759aac6947a8b6b0ead1ffc7724f43ca606b
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2020-08-04 11:14:48 -0700

    Restrict tokenizer creation

--- src/common/tokenizer.h
+++ src/common/tokenizer.h
@@ -12,6 +12,13 @@ namespace iris {
 
 class Tokenizer {
  public:
+  std::string ResolvePath(absl::string_view file_path) const;
+  void Include(absl::string_view file_path);
+
+  absl::optional<absl::string_view> Peek();
+  absl::optional<absl::string_view> Next();
+
+ private:
   Tokenizer() = default;
   Tokenizer(const Tokenizer&) = delete;
   Tokenizer& operator=(const Tokenizer&) = delete;
@@ -21,13 +28,6 @@ class Tokenizer {
   static Tokenizer CreateFromFile(absl::string_view file_path);
   static Tokenizer CreateFromStream(std::istream& stream);
 
-  std::string ResolvePath(absl::string_view file_path) const;
-  void Include(absl::string_view file_path);
-
-  absl::optional<absl::string_view> Peek();
-  absl::optional<absl::string_view> Next();
-
- private:
   std::stack<std::pair<std::reference_wrapper<std::istream>,
                        std::unique_ptr<std::istream>>>
       m_streams;
@@ -35,6 +35,8 @@ class Tokenizer {
   std::string m_peeked;
   absl::optional<bool> m_peeked_valid;
   absl::optional<std::string> m_search_root;
+
+  friend class Parser;
 };
 
 }  // namespace iris
